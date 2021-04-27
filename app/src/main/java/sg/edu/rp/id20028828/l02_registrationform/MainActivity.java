1861efe94a76eb66c6436bc4053766962adf3ef8
20028828 <20028828@myrp.edu.sg>
2021-04-27 23:47:21 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/id20028828/l02_registrationform/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.id20028828.l02_registrationform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise1);
+    }
+}
